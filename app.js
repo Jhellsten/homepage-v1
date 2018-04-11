5679988b0ea424c78e4f82fcebf42ff67b882a05
Janne Hellsten <hellsten@live.com>
2018-04-11 23:19:38 +0300

    First init

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require("express");
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    methodOverride = require("method-override")
+    Blog = require("./models/blog"),
+    Comment = require("./models/comment"),
+    User = require("./models/user"),
+    commentRoutes = require("./routes/comment"),
+    blogRoutes = require("./routes/blogs"),
+    authRoutes = require("./routes/index"),
+    middlewareObj = require("./middleware")
+    
+
+mongoose.connect("mongodb://localhost/hellsten");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// Passport configuration
+app.use(require("express-session")({
+	secret: "Once again its going to gain once again",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
+
+app.use("/", authRoutes);
+app.use("/blogs", blogRoutes);
+app.use("/blogs/:id/comments", commentRoutes);
+
+app.use('/', express.static(__dirname + '/www')); // redirect root
+
+
+// app.listen(process.env.PORT, process.env.IP);
+
+app.listen(3000, function(){
+	console.log("The server has started!");
+});
---
+++ models/user.js
@@ -0,0 +1,11 @@
+var mongogoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
